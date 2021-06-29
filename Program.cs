267ef8912207f9f9ab51d6c6c9bb0d969cc193bd
Marzie Sangtarash <m.sangtarash@outlook.com>
2021-06-29 12:53:45 +0430

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ReceiptTemplateSkiaSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
